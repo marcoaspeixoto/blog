b9ee725649d747127decc0d5a3fbbe167b832525
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-07-27 22:44:52 -0300

    feat: Criação de blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
+
   resources :posts
   resources :commments, only: %i[create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
