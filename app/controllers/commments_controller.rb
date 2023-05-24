48377f42379472ff61b419be5eb8ef25061269fb
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-05-24 06:54:37 -0300

    feat(comments): controller

---
+++ app/controllers/commments_controller.rb
@@ -0,0 +1,10 @@
+class CommentsController < ApplicationController
+  def create
+    #comment = Comment.create!(coments_params)
+  end
+
+  private
+  def comments_params
+    params.require(:comment).permit(:post_id, :author, :body)
+  end
+end
