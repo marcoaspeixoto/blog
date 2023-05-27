476a09fce792720577e75a48f088a95e34d6cd72
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-05-26 22:48:59 -0300

    refactor(commments): change name

--- app/controllers/commments_controller.rb
+++ app/controllers/commments_controller.rb
@@ -1,11 +1,14 @@
-class CommentsController < ApplicationController
+class CommmentsController < ApplicationController
   def create
     binding.pry
-    #comment = Comment.create!(coments_params)
+    #comment = Comment.create!(comments_params)
   end
 
   private
-  def comments_params
-    params.require(:comment).permit(:post_id, :author, :body)
+  def commments_params
+    params.require(:commment).permit(:post_id, :author, :body)
   end
 end
+
+
+#Consertar nome do arquivo commments_controller (está com 3 m's)
