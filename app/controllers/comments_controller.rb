a875122b0f1c24f50d33772ddfaace295a05f727
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-11-25 22:14:40 -0300

    feat(comments): create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
   def create
-    binding.pry
-    #comment = Comment.create!(comments_params)
+    comment = Comment.create!(comments_params)
+
+    redirect_to comment.post, notice: 'O seu comentário foi enviado.'
   end
 
   private
@@ -9,6 +10,3 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:post_id, :author, :body)
   end
 end
-
-
-#Consertar nome do arquivo commments_controller (está com 3 m's)
