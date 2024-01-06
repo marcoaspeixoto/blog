ba5895b4161067ac979295735da0c9a2c549bb62
Marco Peixoto <marcoaspeixoto@gmail.com>
2024-01-05 23:31:47 -0300

    feat(post): search

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,13 @@
 class Post < ApplicationRecord
+  include PgSearch::Model
   extend FriendlyId
   validates :title, :author, :body, presence: true
 
   has_many :commments
 
   friendly_id :title, use: :slugged
+
+  pg_search_scope :search,
+                  agaist: %i[litle author body],
+                  associated_agaist: { commments: %i[body] }
 end
